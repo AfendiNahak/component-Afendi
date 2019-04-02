fa8b06d5824bac28eee37f5d8b85a2dbefaa9c53
HaruskuaT <mikaeldionitoharbigantara@gmail.com>
2019-04-02 11:53:09 +0700

    Initial commit

---
+++ app/src/main/java/akakom/lab/student/pertemuan5/FormRegistrasi.java
@@ -0,0 +1,13 @@
+package akakom.lab.student.pertemuan5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormRegistrasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_registrasi);
+    }
+}
